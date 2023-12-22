4f44330264d7181c00c580b7e91fe3f041188b90
Jean-Massumi <geanmassumit@hotmail.com>
2023-12-22 19:29:50 -0300

    especificacao do exercicio

---
+++ estruturas_enumerados/problemas/exer6.py
@@ -0,0 +1,40 @@
+# Análise:
+#   Calcular o valor final que o consumidor tem que pagar dado o seu consumo em kilowatts/hora , a tarifa
+#   básica do kilowatt/hora e a bandeira tarifária.
+
+# Definição:
+#   A bandeira tarifária possui 4 tipos de bandeiras:
+#       Bandeira Verde indica condições favoráveis , então a tarifa não sofre acréscimo.
+#       Bandeira Amarela indica condições menor favoráveis, então a tarifa sofre um acréscimo de R$0,01874 Kw/h.
+#       Bandeira Vermelha1 indica condições mais custosas, então a tarifa sofre um acréscimo de R$0,03971 Kw/h.
+#       Bandeirad Vermelha2 indica condições ainda mais custosas, então a tarifa sofre um acréscimo de 
+#       R$0,09492 por cada Kw/h consumido.
+
+#   A tarifa básica e a bandeira tarifária serão representados por valores float.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
