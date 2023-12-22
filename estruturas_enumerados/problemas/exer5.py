30f95df7549351f7f4469fa4b4cbc9bbe14db219
Jean-Massumi <geanmassumit@hotmail.com>
2023-12-22 18:40:52 -0300

    introduzinho a funcao Enum e auto no exercicio.

--- estruturas_enumerados/problemas/exer5.py
+++ estruturas_enumerados/problemas/exer5.py
@@ -4,25 +4,31 @@
 # Definição:
 #   A classificação deverá ser curto, mediano ou longo.
 
+from enum import Enum, auto
 
-def tamanho_nome(nome:str) -> str:
+class Nome(Enum):
+    CURTO = auto()
+    MEDIANO = auto()
+    LONGO = auto()
+
+def tamanho_nome(nome:str) -> Nome:
     '''
         Classificar se um *nome* de uma pessoa é curto, mediano ou longo de acordo com o tamanho dela.
     
     Exemplos:
-    >>> tamanho_nome('Ana')
+    >>> tamanho_nome('Ana').name
     'CURTO'
-    >>> tamanho_nome('Beatriz')
+    >>> tamanho_nome('Beatriz').name
     'MEDIANO'
-    >>> tamanho_nome('Antonieta')
+    >>> tamanho_nome('Antonieta').name
     'LONGO'
     '''
     if len(nome) <= 4:
-        tamanho = 'CURTO'
+        tamanho = Nome.CURTO
     elif len(nome) > 8:
-        tamanho = 'LONGO'
+        tamanho = Nome.LONGO
     else:
-        tamanho = 'MEDIANO'
+        tamanho = Nome.MEDIANO
 
     return tamanho
 
