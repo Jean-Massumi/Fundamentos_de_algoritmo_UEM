72003420512515ceb5f45c287f68d4bc226a23bf
Jean-Massumi <geanmassumit@hotmail.com>
2023-12-23 11:51:34 -0300

    sorteio da mega-sena

--- estruturas_enumerados/treino/mega_sena.py
+++ estruturas_enumerados/treino/mega_sena.py
@@ -1,23 +1,39 @@
 # Análise:
-#   Determinar 15 numeros para serem sorteados de 1 a 25 na lotofacil.
+#   Determinar 6 numeros para serem sorteados de 1 a 60 na Mega-sena.
 
 # Definição:
 #   Os numeros serao representados por valores inteiros positivos.
 
 from random import randint, sample
 
-
 def sorteio(sorteados) -> int:
     '''
-        Determinar quais serao os 15 numeros sortedos na lotofácil.
+        Determinar quais serao os 6 numeros sortedos na Mega-sena.
     '''
 
-    sorteados = sample(range(1,25),15)
+    sorteados = sample(range(1,60),6)
 
     return sorted(sorteados)
 
 
 print(sorteio(''))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
