25187af9efb3a9175583153d7decaf3b9e6f16e1
Jean-Massumi <geanmassumit@hotmail.com>
2023-12-25 14:51:04 -0300

    finalizacao do exercicio do tabuleiro

--- estruturas_enumerados/problemas/exer11.py
+++ estruturas_enumerados/problemas/exer11.py
@@ -1,5 +1,5 @@
 # Análise:
-#   Determinar quantas casas de um tabuleiro um personagem pode andar dependendo da posição que está e
+#   Determine quantas casas de um tabuleiro um personagem pode andar dependendo da posição que está e
 #   da direção que ele está.
 
 # Definição:
@@ -9,27 +9,39 @@
 #   por uma string.
 
 from dataclasses import dataclass
-from enum import Enum,auto
-
-class Direcao(Enum):
-    NORTE = auto()
-    SUL = auto()
-    LESTE = auto()
-    OESTE = auto()
 
 @dataclass
 class Personagem:
     POSICAO_LINHA : int
     POSICAO_COLUNA : int
-    DIRECAO : Direcao
+    DIRECAO : str
 
-# def avancar_direcao(p:Personagem) ->int:
+def avancar_direcao(p:Personagem) -> str:
+    '''
+        Determinar quantas casas maximas um personagem pode avancar depende direção que ele está virado.
 
+    Exemplos:
+    >>> avancar_direcao(Personagem(3, 7, 'Oeste'))
+    'O personagem pode andar 6 casas para o Oeste'
+    >>> avancar_direcao(Personagem(9, 2, 'Sul'))
+    'O personagem pode andar 8 casas para o Sul'
+    >>> avancar_direcao(Personagem(4, 1, 'Leste'))
+    'O personagem pode andar 9 casas para o Leste'
+    >>> avancar_direcao(Personagem(5, 8, 'Norte'))
+    'O personagem pode andar 5 casas para o Norte'
 
+    '''
 
+    if p.DIRECAO == 'Norte':
+        passos = f'O personagem pode andar {10-p.POSICAO_LINHA} casas para o Norte'
+    elif p.DIRECAO == 'Sul':
+        passos = f'O personagem pode andar {p.POSICAO_LINHA - 1} casas para o Sul'
+    elif p.DIRECAO == 'Leste':
+        passos = f'O personagem pode andar {10-p.POSICAO_COLUNA} casas para o Leste'
+    else:
+        passos = f'O personagem pode andar {p.POSICAO_COLUNA - 1} casas para o Oeste'
 
-
-
+    return passos
 
 
 
