10fd158ea02f94962e7c705874b802fced3517eb
Jean-Massumi <geanmassumit@hotmail.com>
2023-12-24 22:26:29 -0300

    extraindo as informacoes do enunciado

---
+++ estruturas_enumerados/problemas/exer10.py
@@ -0,0 +1,42 @@
+# Análise:
+#   Determinar o melhor candidato classificado em uma prova de 0 a 100, caso haja empate , o candidato com menor 
+#   inscrição é classificado primeiro.
+
+# Definição:
+#   Os pontos da prova e a inscrição do candidato serão representados por valores inteiros.
+
+from dataclasses import dataclass
+
+@dataclass
+class Candidato_1:
+    INSCRICAO : int
+    PONTOS : int
+
+@dataclass
+class Candidato_2:
+    INSCRICAO : int
+    PONTOS : int
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
