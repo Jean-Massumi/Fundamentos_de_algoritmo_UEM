8b21d0b59b960f522c5d09f7fa239a152690bd47
Jean-Massumi <geanmassumit@hotmail.com>
2023-12-27 15:24:07 -0300

    finalizacao parte 2

--- estruturas_enumerados/pratica/exer4.py
+++ estruturas_enumerados/pratica/exer4.py
@@ -3,7 +3,7 @@
 #   Projete uma função para cada item abaixo:
 #   a- Indique a direção oposta de uma direção.
 #   b- Indique qual é a direção que está a 90 graus no sentido horário de outra direção.
-#   c- Indique qual é a direção que está a 90 graus no sentido anti-horário de outro direção. Use o item b
+#   c- Indique qual é a direção que está a 90 graus no sentido anti-horário de outra direção. Use o item b
 #      para fazer a implementação(não use seleção nem operações aritmeticas nessa implementação)
 
 from enum import Enum,auto
@@ -41,9 +41,31 @@ def direcao_oposta(d:Direcoes) -> Direcoes:
     return oposta
 
 
+def direcao_90_graus(d:Direcoes) -> Direcoes:
+    '''
+        Indicar a direção de 90 graus no sentido horário de outra direçao.
 
+    Exemplos:
+    >>> direcao_90_graus(Direcoes.NORTE).name
+    'LESTE'
+    >>> direcao_90_graus(Direcoes.SUL).name
+    'OESTE'
+    >>> direcao_90_graus(Direcoes.LESTE).name
+    'SUL'
+    >>> direcao_90_graus(Direcoes.OESTE).name
+    'NORTE'
+    '''
 
+    if d == Direcoes.NORTE:
+        direcao_90 = Direcoes.LESTE
+    elif d == Direcoes.SUL:
+        direcao_90 = Direcoes.OESTE
+    elif d == Direcoes.LESTE:
+        direcao_90 = Direcoes.SUL
+    else:
+        direcao_90 = Direcoes.NORTE
 
+    return direcao_90
 
 
 
