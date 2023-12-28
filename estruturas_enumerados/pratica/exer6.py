484418297c9cf359554bf092422831d4a9e76453
Jean-Massumi <geanmassumit@hotmail.com>
2023-12-28 13:58:10 -0300

    Parte 2

--- estruturas_enumerados/pratica/exer6.py
+++ estruturas_enumerados/pratica/exer6.py
@@ -22,12 +22,12 @@ class Data:
 
 def ultimo_dia_ano(d:Data) -> bool:
     '''
-        Verificar uma data para saber se é o ultimo dia do ano.
+        Verificar uma data para saber se é o ultimo dia do ano. Se for devolve *True* senão *False*
     
     Exemplos:
-    >>> ultimo_dia_ano(Data(31,12,23))
+    >>> ultimo_dia_ano(Data(31,12,2023))
     True
-    >>> ultimo_dia_ano(Data(31,10,22))
+    >>> ultimo_dia_ano(Data(31,10,2022))
     False
     '''
     verificar = False
@@ -37,17 +37,36 @@ def ultimo_dia_ano(d:Data) -> bool:
     return verificar
 
 
+def duas_datas(d1:Data, d2:Data) -> bool:
+    '''
+        Verificar se a primeira data vem antes da segunda. Se sim devolve *True* senão *False*.
 
+        Em caso de as datas forem iguais, devolve *False*.
+    
+    >>> duas_datas(Data(14,7,1987), Data(15,9,2000))
+    True
+    >>> duas_datas(Data(30,8,2005), Data(23,12,2005))
+    True
+    >>> duas_datas(Data(13,3,2009), Data(15,3,2009))
+    True
+    >>> duas_datas(Data(23,11,2015), Data(9,1,2003))
+    False
+    >>> duas_datas(Data(19,12,2030), Data(19,12,2030))
+    False
+    >>> duas_datas(Data(29,12,2023), Data(14,12,2023))
+    False
+    >>> duas_datas(Data(1,5,2030), Data(19,4,2030))
+    False
 
+    '''
 
+    verificar = False
 
-
-
-
-
-
-
-
-
+    if (d1.ANO == d2.ANO and d1.MÊS == d2.MÊS and d1.DIA < d2.DIA) or \
+                (d1.ANO == d2.ANO and d1.MÊS < d2.MÊS) or \
+                        (d1.ANO < d2.ANO) :
+        verificar = True
+   
+    return verificar
 
 
