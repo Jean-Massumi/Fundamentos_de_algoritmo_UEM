113514c94493f6fcd2694929120521ee184b956e
Jean-Massumi <geanmassumit@hotmail.com>
2023-12-28 22:57:42 -0300

    Finalizacao

--- estruturas_enumerados/pratica/exer7.py
+++ estruturas_enumerados/pratica/exer7.py
@@ -38,8 +38,6 @@ def mega_pixels(r:Resolucoes) -> float:
     '''
         Determinar quantos mega pixels uma imagem tem ,dada a sua resolução
 
-        O número de megapixel pode ser caculado multiplicando-se a altura e largura e dividindo-se por
-        1 milhão.
     Exemplos:
     >>> mega_pixels(Resolucoes(1080,1920))
     2.0736
@@ -51,13 +49,49 @@ def mega_pixels(r:Resolucoes) -> float:
     return r.ALTURA * r.LARGURA / 1000000
 
 
+def duas_resolucoes(imagem:Resolucoes, tela:Resolucoes):
+    '''
+        Produz True se a imagem pode ser exibida na tela sem a necessidade de rotacao ou reduzao.
+        Produz False caso contrario.
+
+    Exemplos:
+    >>> duas_resolucoes(Resolucoes(2500,1700), Resolucoes(1820,1580))
+    False
+    >>> duas_resolucoes(Resolucoes(1000,800), Resolucoes(1000,800))
+    True
+    >>> duas_resolucoes(Resolucoes(1350,600), Resolucoes(1100,990))
+    False
+    >>> duas_resolucoes(Resolucoes(540,330), Resolucoes(770,430))
+    True
+    >>> duas_resolucoes(Resolucoes(1450,2600), Resolucoes(1600,2300))
+    False
+    '''
+    return imagem.ALTURA <= tela.ALTURA and imagem.LARGURA <= tela.LARGURA
 
 
 
+def aspecto_resolucao(r:Resolucoes):
+    '''
+        Determinar o aspecto de uma resolucao r.
 
+        Se a largura * 4 == altura * 3 , devolve o aspecto 4:3 da resolucao.
+        Se a largura * 16 == altura * 9 , devolve o aspecto 16:9 da resolucao.
+        Senao nao for nenhuma devolve outro aspecto da resolucao.
+    
+    Exemplos:
+    >>> aspecto_resolucao(Resolucoes(2160, 3840 )).name
+    'A16x9'
+    >>> aspecto_resolucao(Resolucoes(480, 640)).name
+    'A4x3'
+    >>> aspecto_resolucao(Resolucoes(1100, 1100)).name
+    'OUTRO'
+    '''
 
+    if r.ALTURA * 4 == r.LARGURA * 3:
+        aspecto = Aspecto.A4x3
+    elif r.ALTURA * 16 == r.LARGURA * 9:
+        aspecto = Aspecto.A16x9
+    else:
+        aspecto = Aspecto.OUTRO
 
-
-
-
-
+    return aspecto
