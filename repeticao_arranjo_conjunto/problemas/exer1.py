4ff8e2af0f6c73767c50910d6c172497193bc5fc
Jean-Massumi <geanmassumit@hotmail.com>
2023-12-29 18:53:34 -0300

    especificao do exercicio

---
+++ repeticao_arranjo_conjunto/problemas/exer1.py
@@ -0,0 +1,45 @@
+# Análise:
+#   Eliminar os valores incorretos de uma sequência de valores produzidas pelo equipamento.
+
+# Denifição:
+#   A lista de sequencia será representado por valores Reais.
+
+def eliminar_negativos(sequencia:list) -> list :
+    '''
+        Devolve a *sequencia* sem os numeros negativos em uma outra lista
+
+    >>> eliminar_negativos([3, -4, -2, 21, -8, 34, 76, 23, -1])   
+    [3, 21, 34, 76, 23]
+    >>> eliminar_negativos([-0.7, -101, 5, 41, -16, -0.234, 0.1, 0.45])
+    [5, 41, 0.1, 0.45]
+    '''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
