6b838ba779b6bc08d606e47ea6e9f5317d44bbb4
Jean-Massumi <geanmassumit@hotmail.com>
2023-12-29 21:19:23 -0300

    Finalização

--- repeticao_arranjo_conjunto/problemas/exer3.py
+++ repeticao_arranjo_conjunto/problemas/exer3.py
@@ -16,7 +16,29 @@ class Conta:
     GASTOS : list[float]
     RECEBIMENTOS : list[float]
 
+def controle_gasto(c:Conta) -> str:
+    '''
+        Calcular quanto Pedro deve doar.
 
+        Se a *Saldo* < 0 , deve doar +10 reias
+    
+    Exemplos:
+    >>> controle_gasto(Conta(200, [143.7, 61.9, 9.5, 56.8], [12, 6.5, 22.3]))
+    'Pedro deverá doar 10 reais'
+    >>> controle_gasto(Conta(300, [143.7, 61.9, 9.5, 56.8], [12, 6.5, 22.3]))
+    'Pedro deverá doar 0 reais'
+    '''
+    doacao = 0
+
+    for saida in c.GASTOS:
+        c.SALDO -= saida
+    for entrada in c.RECEBIMENTOS:
+        c.SALDO += entrada
+
+    if c.SALDO < 0:
+        doacao += 10
+
+    return f'Pedro deverá doar {doacao} reais'
 
 
 
