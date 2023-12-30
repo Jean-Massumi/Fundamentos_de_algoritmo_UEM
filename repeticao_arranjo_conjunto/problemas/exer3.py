aa72bf0f8e390cf701a1a301b36847e402b4fa7d
Jean-Massumi <geanmassumit@hotmail.com>
2023-12-29 21:18:40 -0300

    especificacao do exercicio

--- repeticao_arranjo_conjunto/problemas/exer3.py
+++ repeticao_arranjo_conjunto/problemas/exer3.py
@@ -1,7 +1,20 @@
 # Análise:
-#   
+#   Determinar quantos reais Pedro deverá usar em um certo período.
 
+# Definição:
+#   O controle de gastos de Pedro tem tres componentes(Saldo da conta, gastos ,recebimentos) por isso
+#   será repesentado por uma estrutura.
 
+from dataclasses import dataclass
+
+@dataclass
+class Conta:
+    '''
+        Indica as entradas e saidas no saldo da conta.
+    '''
+    SALDO : float
+    GASTOS : list[float]
+    RECEBIMENTOS : list[float]
 
 
 
